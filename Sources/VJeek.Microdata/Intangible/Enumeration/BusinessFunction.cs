ce83af25c1544491bd902a3743171e59ecc6ac01
Nikolay Safonov <nikolay.safonov@redkassa.ru>
2015-04-07 19:54:39 +0300

    added BusinessFunction

---
+++ Sources/VJeek.Microdata/Intangible/Enumeration/BusinessFunction.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VJeek.Microdata.Intangible.Enumeration
+{
+	[Flags]
+	public enum BusinessFunction
+	{
+		ConstructionInstallation,
+		Dispose,
+		LeaseOut,
+		Maintain,
+		ProvideService,
+		Repair,
+		Sell,
+		Buy
+	}
+}
